a2c618c8a2c90cd3387759e14048691abe4eeeae
Connie He <conniehe@D-69-91-132-125.dhcp4.washington.edu>
2017-04-13 14:13:24 -0700

    New

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,27 +7,32 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library("fueleconomy")
 
 # You should have have access to the `vehicles` data.frame
-
+View(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+car1997<-vehicles[vehicles$year==1997,]
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(car1997$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+wheel2<-vehicles[vehicles$drive=='2-Wheel Drive' & vehicles$cty>20,]
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+wheel2$make[wheel2$hwy == min(wheel2$hwy)]
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
+compete<-function(year,make){
+  newdf<-vehicles[vehicles$year==year & vehicles$make==make,]
+  ans<-newdf[newdf$hwy == max(newdf$hwy),]
+  return(ans)
+}
 
 
 # What was the most efficient honda model of 1995?
-
+compete(1995,'Honda')
 
